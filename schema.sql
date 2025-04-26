f59b8afc0927cf97ddde75acbae848e8e960698c
pikachuren <pikachuren@tencent.com>
2025-04-26 20:54:17 +0800

    Updated20250426205417

--- schema.sql
+++ schema.sql
@@ -1,10 +1,13 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 
 
-CREATE TABLE "Users"
+CREATE TABLE Users
 (
-    mail TEXT NOT NULL PRIMARY KEYUNIQUE,
-    flag TEXT (1) DEFAULT (0) NOT NULL,
+    mail TEXT NOT NULL
+        PRIMARY KEY
+        UNIQUE,
+    flag TEXT (1) DEFAULT (0)
+                  NOT NULL,
     code TEXT (8),
     keys TEXT,
     pass TEXT,
@@ -13,27 +16,30 @@ CREATE TABLE "Users"
 );
 
 
-CREATE TABLE "Apply"
+CREATE TABLE Apply
 (
-    "uuid" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "user" INTEGER NOT NULL,
-    "flag" INTEGER NOT NULL,
-    "time" TEXT    NOT NULL,
-    "main" TEXT    NOT NULL,
-    "list" TEXT    NOT NULL,
-    "keys" TEXT    NOT NULL,
-    "cert" TEXT    NOT NULL
+    uuid INTEGER NOT NULL
+        PRIMARY KEY AUTOINCREMENT,
+    user INTEGER NOT NULL,
+    flag INTEGER NOT NULL,
+    time TEXT    NOT NULL,
+    main TEXT    NOT NULL,
+    list TEXT    NOT NULL,
+    keys TEXT    NOT NULL,
+    cert TEXT    NOT NULL
 );
 
-CREATE TABLE "Authy"
+
+CREATE TABLE Authy
 (
-    "user" INTEGER NOT NULL,
-    "type" INTEGER NOT NULL,
-    "hash" TEXT    NOT NULL,
-    "time" TEXT    NOT NULL,
-    "name" TEXT    NOT NULL,
-    "data" TEXT    NOT NULL
+    user INTEGER NOT NULL,
+    type INTEGER NOT NULL,
+    hash TEXT    NOT NULL,
+    time TEXT    NOT NULL,
+    name TEXT    NOT NULL,
+    data TEXT    NOT NULL
 );
+
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Users_mail_key" ON "Users" ("mail");
