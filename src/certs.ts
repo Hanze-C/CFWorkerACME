b2eb1a6c1ba704d01496e75ec13a96de9e5e1efc
pikachuim <pikachuim@qq.com>
2025-08-18 16:50:57 +0800

    add(ci): edgeone deploy

--- src/certs.ts
+++ src/certs.ts
@@ -9,8 +9,8 @@ import {hmacSHA2} from "./users";
 
 
 const acme_url_map: Record<string, any> = {
-    // "lets-encrypt": acme.directory.letsencrypt.production,
-    "lets-encrypt": "https://encrys.524228.xyz/directory",
+    "lets-encrypt": acme.directory.letsencrypt.production,
+    // "lets-encrypt": "https://encrys.524228.xyz/directory",
     "google-trust": acme.directory.google.production,
     "bypass-trust": acme.directory.buypass.production,
     "zeroca-trust": acme.directory.zerossl.production,
